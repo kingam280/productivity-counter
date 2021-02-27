5f5ad191041b3599b54585e8f78d6f91dbe5b881
Kinga Mamak <kingam280@gmail.com>
2021-02-27 19:03:38 +0100

    Implement react router

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,42 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import Menu from './Menu'
 import Footer from './Footer'
 import Timer from './Timer'
 import Stats from './Stats'
 import Settings from './Settings'
+import NotFound from './NotFound'
 import '../styles/App.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('timer')
-
-  const renderCurrentPage = () => {
-    if (currentPage === 'timer') return <Timer />
-    else if (currentPage === 'stats') return <Stats />
-    else if (currentPage === 'settings') return <Settings />
-  }
-
   return (
-    <div className="App">
-      <Menu />
-      {renderCurrentPage()}
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Menu />
+        <Switch>
+          <Route exact path="/">
+            <Timer />
+          </Route>
+          <Route path="/timer">
+            <Timer />
+          </Route>
+          <Route path="/stats">
+            <Stats />
+          </Route>
+          <Route path="/settings">
+            <Settings />
+          </Route>
+          <Route>
+            <NotFound />
+          </Route>
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Menu() {
     
     return (
       <div className="App">
-        Hello, I'm Menu!
+        <nav>
+            <Link to="/timer">Timer</Link>
+            <Link to="/stats">Stats</Link>
+            <Link to="/settings">Settings</Link>
+        </nav>
       </div>
     );
   }
