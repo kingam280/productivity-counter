9d4d8faa1ade14636f83b07f1a1cf47c22eb690e
Kinga Mamak <kingam280@gmail.com>
2021-03-12 14:45:11 +0100

    Repair date displaying

--- src/components/Stats/Record.js
+++ src/components/Stats/Record.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Record = ({timestamp, label, timeInMinutes}) => {
     const date = new Date(timestamp)
     const day = date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
-    const month = date.getMonth() < 10 ? '0' + date.getMonth() : date.getMonth()
+    const month = date.getMonth() < 9 ? '0' + (date.getMonth() + 1) : (date.getMonth() + 1)
     const year = date.getFullYear()
     
     return (
