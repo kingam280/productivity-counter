4733c55bc5f370d136f711ff3154ec68a004bdfd
Kinga Mamak <kingam280@gmail.com>
2021-03-20 17:14:04 +0100

    Remove console logs

--- src/utils/saveData.js
+++ src/utils/saveData.js
@@ -8,13 +8,11 @@ const saveData = (userId, startTime, label) => {
     }
 
     if (userId) {
-        console.log(userId + 'is logged')
         axios
             .post(`/${userId}.json`, data)
             .then(res => console.log('Successfully added to database'))
             .catch(error => console.log(error))
     } else {
-        console.log('unknown user')
         const array = JSON.parse(localStorage.getItem(data)) || []
         array.push(data)
         localStorage.setItem('data', JSON.stringify(array))
