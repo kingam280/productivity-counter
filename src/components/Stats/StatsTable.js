ab113d755ba1585c903559de3fae874c4bac3ffe
Kinga Mamak <kingam280@gmail.com>
2021-03-27 11:33:15 +0100

    Add pagination to stats table

--- src/components/Stats/StatsTable.js
+++ src/components/Stats/StatsTable.js
@@ -1,46 +1,71 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import Record from './Record'
 import Loading from '../Loading/Loading'
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'next':
+      if(state.stop >= action.length) return state
+      return {start: state.start + 5, stop: state.stop + 5};
+    case 'prev':
+      if (state.start === 0) return state
+      return {start: state.start - 5, stop: state.stop - 5};
+    default:
+      throw new Error();
+  }
+}
+
 export default function StatsTable({ records, loading }) {
+  const [state, dispatch] = useReducer(reducer, {
+    start: 0,
+    stop: 4
+  });
 
-    const displayTable = () => {
-      if (loading) {
-        return <Loading />
-      } else if (records.length > 0) {
-        return (
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Label</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-            <tbody>
-              {records.map( record => <Record timestamp={record.timestamp} label={record.label} timeInMinutes={record.timeInMinutes} key={record.timestamp} /> ) }
-            </tbody>
-          </table>
-        )
-      } else {
-        return (
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Label</th>
-                <th>Time</th>
-              </tr>
-            </thead>
-          </table>
-        )
-      }
+  const displayTable = () => {
+    if (loading) {
+      return <Loading />
+    } else if (records.length > 0) {
+      return (
+        <>
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Label</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+          <tbody>
+            {records.map( (record, index) => {
+              if (index >= state.start && index <= state.stop) return <Record timestamp={record.timestamp} label={record.label} timeInMinutes={record.timeInMinutes} key={record.timestamp} />
+              else return null 
+            }) }
+          </tbody>
+        </table>
+        <button className="go-to" onClick={() => dispatch({type: 'prev'}) }>{`<`}</button>
+        <button className="go-to" onClick={() => dispatch({type: 'next', length: records.length}) }>{`>`}</button>
+        </>
+      )
+    } else {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Label</th>
+              <th>Time</th>
+            </tr>
+          </thead>
+        </table>
+      )
     }
+  }
 
-    return (
-      <div className="latest-sessions">
-        <h3>Latest sessions</h3>
-        {displayTable()}
-      </div>
-    );
+  return (
+    <div className="latest-sessions">
+      <h3>Latest sessions</h3>
+      {displayTable()}
+      
+    </div>
+  );
 }
